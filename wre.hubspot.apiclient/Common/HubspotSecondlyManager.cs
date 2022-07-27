c799f274a8876da4f72549b5cf69bbffe47b9b21
Prado, Michael <mprado@wrenvironmental.com>
2022-07-27 15:46:32 -0400

    - Final version

--- wre.hubspot.apiclient/Common/HubspotSecondlyManager.cs
+++ wre.hubspot.apiclient/Common/HubspotSecondlyManager.cs
@@ -1,37 +1,36 @@
-﻿namespace wre.hubspot.apiclient.Common;
+﻿using Timer = System.Timers.Timer;
+
+namespace wre.hubspot.apiclient.Common;
 
 public class HubspotSecondlyManager : DelegatingHandler
 {
-    private ManualResetEventSlim threadController;
-    private readonly System.Timers.Timer timer;
+    private readonly ManualResetEventSlim _threadController;
 
     public HubspotSecondlyManager(int internalInSeconds = 10)
     {
-        threadController = new ManualResetEventSlim();
-        timer = new System.Timers.Timer(internalInSeconds * 60);
+        _threadController = new ManualResetEventSlim();
+        var timer = new Timer(internalInSeconds * 1000);
         timer.Elapsed += Timer_Elapsed;
+        timer.Start();
     }
 
     private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
     {
-        threadController.Set();
+        _threadController.Set();
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         var response = await base.SendAsync(request, cancellationToken);
-        var remaining = 0;
-        
-        response.Headers.TryGetValues("X-HubSpot-RateLimit-Remaining", out var r);
-        if (r != null && r.TryGetNonEnumeratedCount(out var qtd3) && qtd3 == 1)
-        {
-            remaining = Convert.ToInt32(r.First());
-        }
+        if (!response.Headers.Contains("X-HubSpot-RateLimit-Remaining")) return response;
 
-        if (remaining <= 10)
-        {
-            threadController.Wait(cancellationToken);
-        }
+        var rateLimit = response.Headers.GetValues("X-HubSpot-RateLimit-Remaining").FirstOrDefault();
+        if (rateLimit == null) return response;
+        var remaining = Convert.ToInt32(rateLimit);
+        if (remaining > 10) return response;
+        Console.WriteLine("------------------ Awaiting for Secondly Rate Limit reset ------------------ ");
+        if (_threadController.IsSet) _threadController.Reset();
+        _threadController.Wait(cancellationToken);
 
         return response;
     }
