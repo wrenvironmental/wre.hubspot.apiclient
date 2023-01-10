023483fc27d61216b9727aaa52f154730727e2a5
Prado, Michael <mprado@wrenvironmental.com>
2023-01-10 14:48:44 -0500

    - Changed SecondlyManager class to account for requests per second and requests per 10 seconds

--- wre.hubspot.apiclient/Common/HubspotSecondlyManager.cs
+++ wre.hubspot.apiclient/Common/HubspotSecondlyManager.cs
@@ -1,37 +1,81 @@
-﻿using Timer = System.Timers.Timer;
+﻿using Microsoft.Extensions.Logging;
+using Timer = System.Timers.Timer;
 
 namespace wre.hubspot.apiclient.Common;
 
 public class HubspotSecondlyManager : DelegatingHandler
 {
-    private readonly ManualResetEventSlim _threadController;
+    private readonly SemaphoreSlim _perSecondThreadManager;
+    private readonly SemaphoreSlim _perTenSecondsThreadManager;
+    private readonly Timer _perSecondTimer = new Timer(1000);
+    private readonly Timer _perTenSecondsTimer = new Timer(10000);
+    private readonly ILogger<HubspotSecondlyManager>? _logger;
+    private readonly object _lock = new object();
 
-    public HubspotSecondlyManager(int internalInSeconds = 10)
+    public int MaxCallsPerSecond { get; }
+    public int MaxCallsEveryTenSeconds { get; }
+
+    public HubspotSecondlyManager(ILogger<HubspotSecondlyManager> logger, int maxCallsPerSecond, int maxCallsEveryTenSeconds)
+        : this(maxCallsPerSecond, maxCallsEveryTenSeconds)
     {
-        _threadController = new ManualResetEventSlim();
-        var timer = new Timer(internalInSeconds * 1000);
-        timer.Elapsed += Timer_Elapsed;
-        timer.Start();
+        _logger = logger;
     }
 
-    private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    public HubspotSecondlyManager(int maxCallsPerSecond, int maxCallsEveryTenSeconds)
     {
-        _threadController.Set();
+        _perSecondThreadManager = new SemaphoreSlim(maxCallsPerSecond, maxCallsPerSecond);
+        _perTenSecondsThreadManager = new SemaphoreSlim(maxCallsEveryTenSeconds, maxCallsEveryTenSeconds);
+
+        _perSecondTimer.Elapsed += OneSecond_Elapsed;
+        _perSecondTimer.Start();
+
+        _perTenSecondsTimer.Elapsed += TenSeconds_Elapsed;
+        _perTenSecondsTimer.Start();
+        MaxCallsPerSecond = maxCallsPerSecond;
+        MaxCallsEveryTenSeconds = maxCallsEveryTenSeconds;
+    }
+
+    private void OneSecond_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        lock (_lock)
+        {
+            if (_perSecondThreadManager.CurrentCount == 0)
+            {
+                try
+                {
+                    _perSecondThreadManager.Release(MaxCallsPerSecond);
+                }
+                catch (SemaphoreFullException) { }
+
+                _logger?.LogInformation($"[Second] - Releasing {MaxCallsPerSecond} positions");
+            }
+        }
+    }
+
+    private void TenSeconds_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        lock (_lock)
+        {
+            if (_perTenSecondsThreadManager.CurrentCount == 0)
+            {
+                try
+                {
+                    _perTenSecondsThreadManager.Release(MaxCallsEveryTenSeconds);
+                }
+                catch (SemaphoreFullException) { }
+
+                _logger?.LogInformation($"[10 Seconds] - Releasing {MaxCallsEveryTenSeconds} positions");
+            }
+        }        
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
+        //Entering both thread managers
+        _perSecondThreadManager.Wait(cancellationToken);
+        _perTenSecondsThreadManager.Wait(cancellationToken);
+
         var response = await base.SendAsync(request, cancellationToken);
-        if (!response.Headers.Contains("X-HubSpot-RateLimit-Remaining")) return response;
-
-        var rateLimit = response.Headers.GetValues("X-HubSpot-RateLimit-Remaining").FirstOrDefault();
-        if (rateLimit == null) return response;
-        var remaining = Convert.ToInt32(rateLimit);
-        if (remaining > 10) return response;
-        Console.WriteLine("------------------ Awaiting for Secondly Rate Limit reset ------------------ ");
-        if (_threadController.IsSet) _threadController.Reset();
-        _threadController.Wait(cancellationToken);
-
         return response;
     }
 }
