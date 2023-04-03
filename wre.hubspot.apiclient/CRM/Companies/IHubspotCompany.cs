d518d467cb52ec8c1bf05ad75e7b15e8e5ed39de
Prado, Michael <mprado@wrenvironmental.com>
2023-04-03 06:03:45 -0400

    - Test

---
+++ wre.hubspot.apiclient/CRM/Companies/IHubspotCompany.cs
@@ -0,0 +1,16 @@
+﻿using wre.hubspot.apiclient.Interfaces;
+
+namespace wre.hubspot.apiclient.CRM.Companies
+{
+    public interface IHubspotCompany : IHubspotEntity
+    {
+        string? Name { get; set; }
+        string? Phone { get; set; }
+        string? Contact { get; set; }
+        string? Address { get; set; }
+        string? Address2 { get; set; }
+        string? City { get; set; }
+        string? State { get; set; }
+        string? Zip { get; set; }
+    }
+}
